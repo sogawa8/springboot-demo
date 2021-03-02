8553614099bfc5971ab7e4ca9d5e4faab3cd7418
Ogawa Satoshi <satoshi.ogawa2@unisys.co.jp>
2021-03-02 10:51:11 +0900

    lombok の導入

--- src/main/java/jp/co/demo/domain/DemoEntity.java
+++ src/main/java/jp/co/demo/domain/DemoEntity.java
@@ -2,9 +2,14 @@ package jp.co.demo.domain;
 
 import javax.persistence.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 @Entity
 public class DemoEntity {
-    
+
     private Long demoId;
     private String demoString;
 

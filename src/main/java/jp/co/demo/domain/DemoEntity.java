6f5172a9f50f177d4ae1ccb9f87486cdd9550c31
Ogawa Satoshi <satoshi.ogawa2@unisys.co.jp>
2021-03-02 10:49:14 +0900

    repository と entity の作成

---
+++ src/main/java/jp/co/demo/domain/DemoEntity.java
@@ -0,0 +1,11 @@
+package jp.co.demo.domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class DemoEntity {
+    
+    private Long demoId;
+    private String demoString;
+
+}
---
+++ src/main/java/jp/co/demo/domain/DemoEntityRepository.java
@@ -0,0 +1,7 @@
+package jp.co.demo.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface DemoEntityRepository extends CrudRepository<DemoEntity, Long> {
+
+}

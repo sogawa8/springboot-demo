59bd5fff511c1783d0393dd46313835ac10212a5
Ogawa Satoshi <satoshi.ogawa2@unisys.co.jp>
2021-03-08 13:59:43 +0900

    form を作成して、画面に渡す

--- src/main/java/jp/co/demo/presentation/DemoController.java
+++ src/main/java/jp/co/demo/presentation/DemoController.java
@@ -1,11 +1,20 @@
 package jp.co.demo.presentation;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.demo.application.DemoScenario;
+
 @Controller
 public class DemoController {
+
+    @Autowired
+    private DemoScenario demoScenario;
 
     @RequestMapping("/get/hoge")
     public String getHoge(Model model) {
@@ -13,4 +22,27 @@ public class DemoController {
         return "hoge";
     }
 
+    @RequestMapping("/get/form")
+    public String getForm(Model model) {
+        DemoForm demoForm = new DemoForm(1L, "hoge");
+        model.addAttribute("demoForm", demoForm);
+        return "demo";
+    }
+
+    @RequestMapping("/get/forms")
+    public String getForms(Model model) {
+        List<DemoForm> demoForms = List.of(new DemoForm(1L, "hoge"));
+        model.addAttribute("demoForms", demoForms);
+        return "demo_list";
+    }
+
+    @RequestMapping("/get/forms/from/db")
+    public String getAllDemoEntity(Model model) {
+        List<DemoForm> demoForms = demoScenario.getAllDemoEntity()
+                                               .stream()
+                                               .map(entity -> new DemoForm(entity.getDemoId(), entity.getDemoString()))
+                                               .collect(Collectors.toList());
+        model.addAttribute("demoForms", demoForms);
+        return "demo_list";
+    }
 }
---
+++ src/main/java/jp/co/demo/presentation/DemoForm.java
@@ -0,0 +1,13 @@
+package jp.co.demo.presentation;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class DemoForm {
+
+    private final long id;
+    private final String value;
+
+}
